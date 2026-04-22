31e5e9391681525790bdff251fe7fdc1defb03a5
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-22 02:24:36 -0300

    feat(web): add shadcn/ui on project

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline font-roboto text-red">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">Teste</Button>
+    </>
   );
 }
