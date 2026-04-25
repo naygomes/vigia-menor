d9e7400b4a2c0e73acace0b62dd6d4c755ac25b3
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 03:58:00 -0300

    feat: protect children routes with ensure Authenticated middleware

--- api/src/routes/children-routes.ts
+++ api/src/routes/children-routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { InMemoryChildrenRepository } from "@/repositories";
 import { ChildrenService } from "@/services";
 import { ChildrenController } from "@/controllers";
+import { ensureAuthenticated } from "@/middlewares";
 
 const childrenRoutes = Router();
 
 const childrenRepository = new InMemoryChildrenRepository();
 const childrenService = new ChildrenService(childrenRepository);
 const childrenController = new ChildrenController(childrenService);
+
+childrenRoutes.use(ensureAuthenticated);
 
 childrenRoutes.get("/", childrenController.findAll);
 childrenRoutes.get("/summary", childrenController.getSummary);
