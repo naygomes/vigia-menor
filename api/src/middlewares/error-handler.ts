953dd68584b3a1186424c9820a300bf113b5b7a5
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 00:52:59 -0300

    feat: create errorHandler middleware

---
+++ api/src/middlewares/error-handler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/errors";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  console.error("[Unhandled Error]:", err);
+  return res.status(500).json({ error: "Erro interno no servidor." });
+}
