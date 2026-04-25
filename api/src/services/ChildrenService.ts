c7527231777f0a6d6a250c501702cc9760a960de
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-24 23:11:43 -0300

    feat: create findAll method children service

---
+++ api/src/services/ChildrenService.ts
@@ -0,0 +1,20 @@
+import {
+  IChild,
+  IChildrenRepository,
+  IFindAllParams,
+  IFindAllResponse,
+  IGetSummaryResponse,
+} from "@/types";
+import { isValidEmail } from "@/utils";
+
+export class ChildrenService {
+  constructor(private childrenRepository: IChildrenRepository) {}
+
+  async findAll(params: IFindAllParams = {}): Promise<IFindAllResponse | null> {
+    const MAX_LIMIT = 50;
+    if (params.limit && params.limit > MAX_LIMIT) {
+      params.limit = MAX_LIMIT;
+    }
+    return this.childrenRepository.findAll(params);
+  }
+}
