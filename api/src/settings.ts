67d175c69bf80ea88140fb38993dc65b68a34d49
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 03:53:28 -0300

    fixup! feat: create login method on auth service

--- api/src/settings.ts
+++ api/src/settings.ts
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
+
+export const JWT_SECRET = process.env.JWT_SECRET;
