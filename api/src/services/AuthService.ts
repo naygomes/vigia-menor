974e33972d6f2e41c5721c64c879e363e6eeeed2
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 15:47:46 -0300

    feat: create register method on auth service

--- api/src/services/AuthService.ts
+++ api/src/services/AuthService.ts
@@ -6,6 +6,34 @@ import { JWT_SECRET } from "@/settings.js";
 export class AuthService {
   constructor(private authRepository: IAuthRepository) {}
 
+  async register(userData: Omit<IUser, "id">): Promise<ILoginResponse> {
+    const existingUser = await this.authRepository.findByEmail(userData.email);
+
+    if (existingUser) {
+      throw new BadRequestError("Este e-mail já está em uso.");
+    }
+
+    if (!JWT_SECRET) {
+      throw new InternalServerError(
+        "CRITICAL ERROR: A variável de ambiente JWT_SECRET não foi configurada no arquivo .env",
+      );
+    }
+
+    const newUser = await this.authRepository.create(userData);
+
+    const token = jwt.sign(
+      { id: newUser.id, email: newUser.email },
+      JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return { token, user: userWithoutPassword };
+  }
+
   async login(email: string, password: string): Promise<ILoginResponse> {
     const user = await this.authRepository.findByEmail(email);
 
