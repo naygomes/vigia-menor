880799a8e76464a5386c0a68466617210c6cce3f
naygomes <nayara.gomes13@poli.ufrj.br>
2026-04-25 22:17:33 -0300

    feat: add settings file with host url and endpoints

---
+++ web/settings.ts
@@ -0,0 +1,9 @@
+export const API_HOST =
+  process.env.NEXT_PUBLIC_API_HOST || "http://localhost:5000";
+
+export const CHILDREN_ROUTE = API_HOST + "/children";
+export const SUMMARY_ENDPOINT = "/summary";
+
+export const REGISTER_ENDPOINT = "/register";
+export const LOGIN_ENDPOINT = "/login";
+export const USER_ENDPOINT = "/users";
